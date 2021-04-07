d804391cb9cd4e5a38609694bb63ace802309ce2
Gustavo Takao <69652926+takaozando@users.noreply.github.com>
2021-04-07 18:18:17 -0300

    Update controller new

--- src/main/java/com/example/ac1/controllers/EventController.java
+++ src/main/java/com/example/ac1/controllers/EventController.java
@@ -4,64 +4,79 @@ import com.example.ac1.entidade.Event;
 import com.example.ac1.services.EventService;
 import com.example.ac1.repositorio.EventRepository;
 
+import facens.example.ac1.dto.DTO;
+import facens.example.ac1.entidade.Event;
+import facens.example.ac1.services.EventService;
 
+import java.net.URI;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-//@RestController
-@Controller
-@RequestMapping("/main")
-public class EventController {
-    
+@RestController
+@RequestMapping("/events")
+public class EventController 
+{
+
     @Autowired
-    EventRepository eventoRepository;
-    EventService eventoService;
-    
+    private EventService service;
 
-    //Retorna View com lista de funcionarios
-    @GetMapping("/eventos")
-    public ModelAndView listarEventos(){
-        ModelAndView mv = new ModelAndView("eventos");//novo modelo e view com o nome da view
-        mv.addObject("eventos",eventoService.getAllEvents());//add lista de objetos funcionario a view
-        return mv;
+    @GetMapping
+    public ResponseEntity<Page<Event>> getAllEvents(@RequestParam(value = "page", defaultValue = "0") Integer page,
+                                                    @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+                                                    @RequestParam(value = "linesPerPage", defaultValue = "6") Integer linesPerPage,
+                                                    @RequestParam(value = "orderBy", defaultValue = "id") String orderBy,
+                                                    @RequestParam(value = "name", defaultValue = "") String name,
+                                                    @RequestParam(value = "place", defaultValue = "") String place,
+                                                    @RequestParam(value = "start_date", defaultValue = "") String startDate,
+                                                    @RequestParam(value = "description", defaultValue = "") String description
+                                                    )
+    {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+        Page<Event> events = service.getEvents(pageRequest, name, place, startDate, description);
+        return ResponseEntity.ok().body(events);
     }
 
-    //Retorna View para criaçao de um novo funcionario
-    @GetMapping("/novofuncionario")
-    public ModelAndView novoFuncionario(){
-        ModelAndView mv = new ModelAndView("novoEvento");
-        mv.addObject("evento", new Event());
-        return mv;
+    @GetMapping("/{id}")
+    public ResponseEntity<Event> getEventById(@PathVariable Long id) 
+    {
+        Event event = service.getEventById(id);
+        return ResponseEntity.ok().body(event);
     }
 
-    //Salva funcionario criado / Atualiza funcionario existente com o mesmo id
-    @PostMapping("/cadastrarevento")
-    public String cadastrarFuncionario(@ModelAttribute Event event){
-        eventoService.saveEvent(event);
-        return "redirect:/main/eventos";
+    @PostMapping
+    public ResponseEntity<Event> insertEvent(@RequestBody DTO insertDTO) 
+    {
+        Event event = service.newEvent(insertDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(event.getId()).toUri();
+        return ResponseEntity.created(uri).body(event);
     }
 
-    //Remove funcionario pelo id passado
-    @GetMapping("/removerevento")
-    public String removerFuncionario(@RequestParam Integer codigo){
-        Event event = eventoService.getEventById(id);
-        eventoService.removeFuncionario(event);
-        //implementar - verificar se existe func - throw exception
-        return "redirect:/main/eventos";
+    @PutMapping("/{id}")
+    public ResponseEntity<Event> updateEvent(@PathVariable Long id, @RequestBody DTO updateDTO) 
+    {
+        Event event = service.attEvent(id, updateDTO);
+        return ResponseEntity.ok().body(event);
     }
 
-    //Retorna view com dados do funcionario para edição
-    @GetMapping("/editarevento")
-    public ModelAndView editarFuncionario(@RequestParam Integer codigo){
-        ModelAndView mv = new ModelAndView("editarEvento");
-        mv.addObject("event", eventoService.getEventById(id));
-        return mv;
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteEvent(@PathVariable Long id) 
+    {
+        service.deleteEvent(id);
+        return ResponseEntity.noContent().build();
     }
-    
+
+   
 }

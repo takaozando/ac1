7f7d38f7341d0680db992d838374265deeacd494
Gustavo Takao <69652926+takaozando@users.noreply.github.com>
2021-04-07 18:19:22 -0300

    Att entity event

--- src/main/java/com/example/ac1/entidade/Event.java
+++ src/main/java/com/example/ac1/entidade/Event.java
@@ -1,22 +1,27 @@
 package com.example.ac1.entidade;
 
+import facens.example.ac1.dto.DTO;
+
 import java.io.Serializable;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+import javax.persistence.Table;
 
 @Entity
-public class Event implements Serializable{
+@Table(name="event")
+public class Event implements Serializable
 {
+
     private static final long serialVersionUID = 1L;
 
-    private int id;
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    
+    private Long id;
     private String name;
     private String description;
     private String place;
@@ -24,83 +29,116 @@ public class Event implements Serializable{
     private LocalDate endDate;
     private LocalTime startTime;
     private LocalTime endTime;
-    private String emailContact;
+    private String email;
 
-    public Event(int id,String name)
+    public Long getId() 
     {
-        
-    }
-
-    public int getId() {
         return id;
     }
-
-    public void setId(int id) {
+    public void setId(long id) 
+    {
         this.id = id;
     }
-
-    public String getName() {
+    public String getName() 
+    {
         return name;
     }
-
-    public void setName(String name) {
+    public void setName(String name) 
+    {
         this.name = name;
     }
-
-    public String getDescription() {
+    public String getDescription() 
+    {
         return description;
     }
-
-    public void setDescription(String description) {
+    public void setDescription(String description) 
+    {
         this.description = description;
     }
-
-    public String getPlace() {
+    public String getPlace() 
+    {
         return place;
     }
-
-    public void setPlace(String place) {
+    public void setPlace(String place) 
+    {
         this.place = place;
     }
-
-    public LocalDate getStartDate() {
+    public LocalDate getStartDate() 
+    {
         return startDate;
     }
-
-    public void setStartDate(LocalDate startDate) {
+    public void setStartDate(LocalDate startDate) 
+    {
         this.startDate = startDate;
     }
-
-    public LocalDate getEndDate() {
+    public LocalDate getEndDate() 
+    {
         return endDate;
     }
-
-    public void setEndDate(LocalDate endDate) {
+    public void setEndDate(LocalDate endDate) 
+    {
         this.endDate = endDate;
     }
-
-    public LocalTime getStartTime() {
+    public LocalTime getStartTime() 
+    {
         return startTime;
     }
-
-    public void setStartTime(LocalTime startTime) {
-        this.startTime = startTime;
+    public void setStartTime(LocalTime starTime) 
+    {
+        this.startTime = starTime;
     }
-
-    public LocalTime getEndTime() {
+    public LocalTime getEndTime() 
+    {
         return endTime;
     }
-
-    public void setEndTime(LocalTime endTime) {
+    public void setEndTime(LocalTime endTime) 
+    {
         this.endTime = endTime;
     }
-
-    public String getEmailContact() {
-        return emailContact;
+    public String getEmail() 
+    {
+        return email;
+    }
+    public void setEmail(String email) 
+    {
+        this.email = email;
     }
 
-    public void setEmailContact(String emailContact) {
-        this.emailContact = emailContact;
+    @Override
+    public int hashCode() 
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (int) (id ^ (id >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) 
+    {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Event other = (Event) obj;
+        if (id != other.id)
+            return false;
+        else
+            return true;
+    }
+
+    public Event(DTO dto) 
+    {
+        this.name = dto.getName();
+        this.description = dto.getDescription();
+        this.place = dto.getPlace();
+        this.startDate = dto.getStartDate();
+        this.endDate = dto.getEndDate();
+        this.startTime = dto.getStartTime();
+        this.endTime = dto.getEndTime();
+        this.email = dto.getEmail();
     }
 
 }
